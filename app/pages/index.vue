2310c953f67674bf9d59d1a104fe7651ba57a223
atlaxt <atlasyigitaydin@gmail.com>
2025-12-06 00:42:53 +0300

    fix

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -35,9 +35,6 @@ async function handleGameOver(finalScore: number) {
           label="GitHub" to="https://github.com/atlaxt/SnakeHub"
         />
         <UColorModeSelect class="w-full mt-2 mb-2" />
-        <div class="text-center text-xs text-muted font-mono">
-          v1.0.0
-        </div>
       </div>
 
       <div class="lg:col-span-8 flex justify-center">
