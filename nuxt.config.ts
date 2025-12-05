6788bd145c9791e78ac55271791e1dc76307591b
atlaxt <atlasyigitaydin@gmail.com>
2025-12-05 23:54:53 +0300

    prod

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
+import process from 'node:process'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/ui', 'nuxt-auth-utils'],
@@ -7,9 +8,20 @@ export default defineNuxtConfig({
   },
 
   nitro: {
+    // local
+    // storage: {
+    //   data: {
+    //     driver: 'fs',
+    //     base: './.data/db',
+    //   },
+    // },
+
+    // production
     storage: {
       data: {
-        driver: 'fs',
+        driver: process.env.NUXT_STORAGE_DRIVER || 'fs',
+        url: process.env.UPSTASH_REDIS_REST_URL,
+        token: process.env.UPSTASH_REDIS_REST_TOKEN,
         base: './.data/db',
       },
     },
